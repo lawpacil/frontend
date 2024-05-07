d3d4f31876afc39f315427f610c298ce78e1acd4
Muhammad Urwatil Wutsqo <urwatilwutsqo16@gmail.com>
2024-05-07 18:59:30 +0700

    Add calendar

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,29 @@
-export default async function Home() {
-  return <div>OK</div>;
+"use client";
+
+import { useState } from "react";
+import { Calendar, Event, momentLocalizer, Views } from "react-big-calendar";
+import moment from "moment";
+
+const localizer = momentLocalizer(moment);
+
+export default function Home() {
+  const [events, setEvents] = useState<Event[]>([]);
+  const [date, setDate] = useState<Date>(new Date());
+  const [view, setView] = useState<any>(Views.WEEK);
+
+  return (
+    <div>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        defaultView="week"
+        timeslots={4}
+        step={15}
+        date={moment(date).toDate()}
+        view={view}
+        onNavigate={(date) => setDate(date)}
+        onView={(view) => setView(view)}
+      />
+    </div>
+  );
 }
